1992e05310768339cd01c15dcf2338aefd6eeb93
Søren Kruse <krusenen@gmail.com>
2017-11-28 19:40:31 +0100

    Move script to separate file

---
+++ src/SitecoreReleaseNotes.Web/wwwroot/js/script.js
@@ -0,0 +1,46 @@
+﻿window.onpopstate = function (event) {
+    vm.query = event.state.query;
+    vm.results = event.state.results;
+};
+
+var router = new VueRouter({
+    mode: "history",
+    routes: []
+});
+
+var vm = new Vue({
+    router,
+    el: "#app",
+    data: {
+        searching: false,
+        query: "",
+        results: []
+    },
+    mounted: function () {
+        if (this.$route.query.q) {
+            this.query = this.$route.query.q;
+            this.search();
+        }
+    },
+    methods: {
+        search() {
+            if (!this.query)
+                return;
+
+            this.searching = true;
+            this.$http.get("/api/search?query=" + this.query).then(response => {
+                vm.searching = false;
+                response.json().then(json => {
+                    vm.results = json;
+
+                    var state = {
+                        results: this.results,
+                        query: this.query
+                    };
+
+                    history.pushState(state, null, "?q=" + this.query);
+                });
+            });
+        }
+    }
+});
